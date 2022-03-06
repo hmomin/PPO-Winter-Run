799b752de96bb001c1149f93463bcb1750350232
Momin Haider <72412509+hmomin@users.noreply.github.com>
2022-03-05 22:27:44 -0500

    Generalized CloudFront URL for loading
    If I ever want to change the CloudFront URL that stores many of the assets, now, I only need to change one line instead of many. Also, I've removed some never-used animations.

--- src/app/game/scenes/load-scene.ts
+++ src/app/game/scenes/load-scene.ts
@@ -1,8 +1,8 @@
 export default class LoadScene extends Phaser.Scene {
     routerUrl: string;
     loadingBar: Phaser.GameObjects.Graphics;
-    loadingText: Phaser.GameObjects.Text;
     assetText: Phaser.GameObjects.Text;
+    assetUrl = "https://d1w49crlcpe45.cloudfront.net/";
 
     constructor() {
         super("load");
@@ -49,174 +49,141 @@ export default class LoadScene extends Phaser.Scene {
     }
 
     loadMusic() {
-        this.load.audio(
-            "music",
-            "https://d1w49crlcpe45.cloudfront.net/audio/winter-run-music.mp3"
-        );
+        this.load.audio("music", this.assetUrl + "audio/winter-run-music.mp3");
     }
 
     loadBackgroundImages() {
         this.load.image(
             "background_1",
-            "https://d1w49crlcpe45.cloudfront.net/background-images/background_1.png"
+            this.assetUrl + "background-images/background_1.png"
         );
         this.load.image(
             "background_2",
-            "https://d1w49crlcpe45.cloudfront.net/background-images/background_2.png"
+            this.assetUrl + "background-images/background_2.png"
         );
     }
 
     loadTextImages() {
         this.load.image(
             "logo",
-            "https://d1w49crlcpe45.cloudfront.net/title-images/winter-run-logo.png"
+            this.assetUrl + "title-images/winter-run-logo.png"
         );
-        this.load.image(
-            "play",
-            "https://d1w49crlcpe45.cloudfront.net/title-images/play.png"
-        );
+        this.load.image("play", this.assetUrl + "title-images/play.png");
         this.load.image(
             "autoplay",
-            "https://d1w49crlcpe45.cloudfront.net/title-images/autoplay.png"
+            this.assetUrl + "title-images/autoplay.png"
         );
         this.load.image(
             "music-on",
-            "https://d1w49crlcpe45.cloudfront.net/title-images/music-on.png"
+            this.assetUrl + "title-images/music-on.png"
         );
         this.load.image(
             "music-off",
-            "https://d1w49crlcpe45.cloudfront.net/title-images/music-off.png"
+            this.assetUrl + "title-images/music-off.png"
         );
     }
 
     loadTiles() {
         // load tilesheet
-        this.load.image(
-            "tilesheet",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/tilesheet.png"
-        );
+        this.load.image("tilesheet", this.assetUrl + "tilemap/tilesheet.png");
         // load tilemap
         this.load.tilemapTiledJSON(
             "map1",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/level1.json"
+            this.assetUrl + "tilemap/level1.json"
         );
         this.loadTilemapImages();
     }
 
     loadTilemapImages() {
-        this.load.image(
-            "apple",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/apple.png"
-        );
+        this.load.image("apple", this.assetUrl + "tilemap/images/apple.png");
         this.load.image(
             "bonus-block",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/bonus-block.png"
+            this.assetUrl + "tilemap/images/bonus-block.png"
         );
-        this.load.image(
-            "bush",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/bush.png"
-        );
-        this.load.image(
-            "coin",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/coin.png"
-        );
+        this.load.image("bush", this.assetUrl + "tilemap/images/bush.png");
+        this.load.image("coin", this.assetUrl + "tilemap/images/coin.png");
         this.load.image(
             "death-sign",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/death-sign.png"
+            this.assetUrl + "tilemap/images/death-sign.png"
         );
         this.load.image(
             "diamond",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/diamond.png"
+            this.assetUrl + "tilemap/images/diamond.png"
         );
         this.load.image(
             "exclam-sign",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/exclam-sign.png"
+            this.assetUrl + "tilemap/images/exclam-sign.png"
         );
-        this.load.image(
-            "fence",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/fence.png"
-        );
+        this.load.image("fence", this.assetUrl + "tilemap/images/fence.png");
         this.load.image(
             "gold-chest",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/gold-chest.png"
+            this.assetUrl + "tilemap/images/gold-chest.png"
         );
         this.load.image(
             "gold-key",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/gold-key.png"
+            this.assetUrl + "tilemap/images/gold-key.png"
         );
-        this.load.image(
-            "heart",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/heart.png"
-        );
+        this.load.image("heart", this.assetUrl + "tilemap/images/heart.png");
         this.load.image(
             "question-sign",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/question-sign.png"
+            this.assetUrl + "tilemap/images/question-sign.png"
         );
         this.load.image(
             "right-sign",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/right-sign.png"
+            this.assetUrl + "tilemap/images/right-sign.png"
         );
-        this.load.image(
-            "shrub",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/shrub.png"
-        );
+        this.load.image("shrub", this.assetUrl + "tilemap/images/shrub.png");
         this.load.image(
             "silver-chest",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/silver-chest.png"
+            this.assetUrl + "tilemap/images/silver-chest.png"
         );
         this.load.image(
             "silver-key",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/silver-key.png"
+            this.assetUrl + "tilemap/images/silver-key.png"
         );
-        this.load.image(
-            "spike",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/spike.png"
-        );
-        this.load.image(
-            "star",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/star.png"
-        );
+        this.load.image("spike", this.assetUrl + "tilemap/images/spike.png");
+        this.load.image("star", this.assetUrl + "tilemap/images/star.png");
         this.load.image(
             "top-right-sign",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/top-right-sign.png"
+            this.assetUrl + "tilemap/images/top-right-sign.png"
         );
         this.load.image(
             "snowman-top-left",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/snowman-top-left.png"
+            this.assetUrl + "tilemap/images/snowman-top-left.png"
         );
         this.load.image(
             "snowman-bottom-left",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/snowman-bottom-left.png"
+            this.assetUrl + "tilemap/images/snowman-bottom-left.png"
         );
         this.load.image(
             "snowman-top-right",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/snowman-top-right.png"
+            this.assetUrl + "tilemap/images/snowman-top-right.png"
         );
         this.load.image(
             "snowman-bottom-right",
-            "https://d1w49crlcpe45.cloudfront.net/tilemap/images/snowman-bottom-right.png"
+            this.assetUrl + "tilemap/images/snowman-bottom-right.png"
         );
     }
 
     loadWinterGirl() {
         this.load.atlas(
             "player",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/winter-girl-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/winter-girl-atlas.json"
+            this.assetUrl + "sprites/winter-girl-spritesheet.png",
+            this.assetUrl + "sprites/winter-girl-atlas.json"
         );
         // load attack winter girl
         this.load.atlas(
             "player-attack",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/winter-girl-attack-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/winter-girl-attack-atlas.json"
+            this.assetUrl + "sprites/winter-girl-attack-spritesheet.png",
+            this.assetUrl + "sprites/winter-girl-attack-atlas.json"
         );
     }
 
     loadExplosions() {
         this.load.atlas(
             "explosion",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/explosion-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/explosion-atlas.json"
+            this.assetUrl + "sprites/explosion-spritesheet.png",
+            this.assetUrl + "sprites/explosion-atlas.json"
         );
     }
 
@@ -224,14 +191,14 @@ export default class LoadScene extends Phaser.Scene {
         // load santa
         this.load.atlas(
             "santa",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/santa-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/santa-atlas.json"
+            this.assetUrl + "sprites/santa-spritesheet.png",
+            this.assetUrl + "sprites/santa-atlas.json"
         );
         // load ice-mage
         this.load.atlas(
             "ice-mage",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/ice-mage-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/ice-mage-atlas.json"
+            this.assetUrl + "sprites/ice-mage-spritesheet.png",
+            this.assetUrl + "sprites/ice-mage-atlas.json"
         );
     }
 
@@ -239,21 +206,21 @@ export default class LoadScene extends Phaser.Scene {
         // load yeti
         this.load.atlas(
             "yeti",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/yeti-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/yeti-atlas.json"
+            this.assetUrl + "sprites/yeti-spritesheet.png",
+            this.assetUrl + "sprites/yeti-atlas.json"
         );
         // load snowman
         this.load.atlas(
             "snowman",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/snowman-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/snowman-atlas.json"
+            this.assetUrl + "sprites/snowman-spritesheet.png",
+            this.assetUrl + "sprites/snowman-atlas.json"
         );
 
         // load crow
         this.load.atlas(
             "crow",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/crow-spritesheet.png",
-            "https://d1w49crlcpe45.cloudfront.net/sprites/crow-atlas.json"
+            this.assetUrl + "sprites/crow-spritesheet.png",
+            this.assetUrl + "sprites/crow-atlas.json"
         );
     }
 
@@ -415,36 +382,6 @@ export default class LoadScene extends Phaser.Scene {
             }),
             frameRate: 10,
         });
-        this.anims.create({
-            key: "santa-brace",
-            frames: this.anims.generateFrameNames("santa", {
-                prefix: "brace_",
-                start: 1,
-                end: 3,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: "santa-skip",
-            frames: this.anims.generateFrameNames("santa", {
-                prefix: "skip_",
-                start: 1,
-                end: 7,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: "santa-jump",
-            frames: this.anims.generateFrameNames("santa", {
-                prefix: "jump_",
-                start: 1,
-                end: 5,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
     }
 
     loadIceMageAnimations() {
@@ -469,36 +406,6 @@ export default class LoadScene extends Phaser.Scene {
             repeat: -1,
         });
         this.anims.create({
-            key: "ice-mage-strike",
-            frames: this.anims.generateFrameNames("ice-mage", {
-                prefix: "strike_",
-                start: 1,
-                end: 7,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: "ice-mage-shoot",
-            frames: this.anims.generateFrameNames("ice-mage", {
-                prefix: "shoot_",
-                start: 1,
-                end: 4,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: "crystal",
-            frames: this.anims.generateFrameNames("ice-mage", {
-                prefix: "crystal_",
-                start: 1,
-                end: 5,
-            }),
-            frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
             key: "ice-mage-walk",
             frames: this.anims.generateFrameNames("ice-mage", {
                 prefix: "walk_",
@@ -506,16 +413,6 @@ export default class LoadScene extends Phaser.Scene {
                 end: 6,
             }),
             frameRate: 5,
-            repeat: -1,
-        });
-        this.anims.create({
-            key: "ice-mage-run",
-            frames: this.anims.generateFrameNames("ice-mage", {
-                prefix: "run_",
-                start: 1,
-                end: 11,
-            }),
-            frameRate: 10,
             repeat: -1,
         });
     }
