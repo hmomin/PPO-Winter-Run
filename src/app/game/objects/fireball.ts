bc5ce27d4a23dbf5f52d71ca4347b13464fc1e8b
Momin Haider <72412509+hmomin@users.noreply.github.com>
2022-04-24 20:21:10 -0500

    Fixed collision error with `Fireball`
    When `Fireball` would collide with a moving target, like `Crow`, it
    wouldn't disappear in some cases, because it would still be moving in
    the same direction. This issue has been fixed now, so that the fireball
    always disappears on collision.

--- src/app/game/objects/fireball.ts
+++ src/app/game/objects/fireball.ts
@@ -3,11 +3,17 @@ import GameScene from "../scenes/game-scene";
 export default class Fireball extends Phaser.Physics.Arcade.Sprite {
     scene: GameScene;
     movingRight: boolean;
+    initialVelocity: number;
     multiplier: integer;
     maxX: number;
 
-    constructor(scene: GameScene, physicsGroup: Phaser.Physics.Arcade.Group) {
-        super(scene, scene.player.x, scene.player.y, "fireball");
+    constructor(
+        scene: GameScene,
+        x: number,
+        y: number,
+        physicsGroup: Phaser.Physics.Arcade.Group
+    ) {
+        super(scene, x, y, "fireball");
         this.scene = scene;
         this.scene.add.existing(this);
         this.scene.physics.world.enableBody(this);
@@ -15,10 +21,11 @@ export default class Fireball extends Phaser.Physics.Arcade.Sprite {
         this.play("fireball", true);
         this.movingRight = !scene.player.flipX;
         this.multiplier = this.movingRight ? 1 : -1;
-        this.body.velocity.x = this.multiplier * 350;
+        this.initialVelocity = this.multiplier * 350;
+        this.body.velocity.x = this.initialVelocity;
         this.maxX = this.movingRight
-            ? this.scene.player.x + 400
-            : this.scene.player.x - 400;
+            ? this.scene.player.x + 700
+            : this.scene.player.x - 700;
         this.setScale(0.25, 0.25);
         this.body.setCircle(40);
     }
@@ -27,7 +34,8 @@ export default class Fireball extends Phaser.Physics.Arcade.Sprite {
         const outOfBounds =
             (this.movingRight && this.x > this.maxX) ||
             (!this.movingRight && this.x < this.maxX);
-        const collided = this.body && this.body.velocity.x === 0;
+        const collided =
+            this.body && this.body.velocity.x !== this.initialVelocity;
         if (outOfBounds || collided) {
             this.destroy();
         }
